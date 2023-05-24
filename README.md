8b927026b92549904b600ddd6aa00b98e224be87
Vahid Hallaji <vahid@hallaji.com>
2023-05-24 19:35:51 +1000

    Add workflow files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Alfred Workflow for Notion
+
+Search your bookmark database in Notion by using tag or name properties. This
+feature allows you to easily retrieve the websites you need by filtering through
+specific keywords or tags that you have assigned to them. With the ability to
+search your bookmarks in Notion directly from Alfred, you can save time and
+streamline your workflow.
+
+## Prerequisits
+
+Set following environment variables in the Alfred workflow before using it.
+
+```text
+NOTION_BOOKMARKS_DATABASE_ID
+NOTION_API_KEY
+```
---
+++ notion-bookmarks.rb
@@ -0,0 +1,110 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'net/http'
+
+api_key = ENV['NOTION_API_KEY']
+database_id = ENV['BOOKMARKS_DATABASE_ID']
+
+if api_key.nil? || api_key.empty?
+  puts JSON.generate({
+    'items' => [{
+      'title' => 'No API token configured',
+      'valid' => false
+    }]
+  })
+
+  return
+end
+
+uri = URI("https://api.notion.com/v1/databases/#{database_id}/query")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+headers = {
+  'Notion-Version' => '2022-06-28',
+  'Authorization' => "Bearer #{api_key}",
+  'Content-Type' => 'application/json'
+}
+
+query = ARGV[0].strip.downcase
+keywords = query.split(' ')
+body = {
+  'sorts' => [
+    {
+      'property' => 'Name',
+      'direction' => 'ascending'
+    }
+  ],
+  'filter' => {
+    'and' => keywords.map do |keyword|
+      {
+        'or' => [
+          {
+            'property' => 'Tags',
+            'multi_select' => {
+              'contains' => keyword.strip.downcase
+            }
+          },
+          {
+            'property' => 'Name',
+            'rich_text' => {
+              'contains' => keyword.strip.downcase
+            }
+          },
+          {
+            'property' => 'Url',
+            'rich_text' => {
+              'contains' => keyword.strip.downcase
+            }
+          }
+        ]
+      }
+    end
+  },
+  'page_size' => 25
+}
+
+request = Net::HTTP::Post.new(uri.path, headers)
+request.body = body.to_json
+response = http.request(request)
+results = JSON.parse(response.body)
+
+if !results.key?('results') || results['results'].empty?
+  puts JSON.generate({
+    'items' => [{
+      'title' => 'Not found…',
+      'valid' => false
+    }]
+  })
+
+  return
+end
+
+items = []
+results['results'].each do |result|
+  url = result.dig('properties', 'Url', 'url') || ''
+  title = result.dig('properties', 'Name', 'title', 0, 'plain_text') || ''
+  tags = result.dig('properties', 'Tags', 'multi_select')&.map { |item| item['name'] } || []
+  subtitle = ''
+  tags.each do |element|
+    if !subtitle.empty?
+      subtitle += ', '
+    end
+    subtitle += element
+  end
+  subtitle += " — #{url}"
+
+  if !title.empty? && !url.empty?
+    items << {
+      'title' => title,
+      'subtitle' => subtitle,
+      'arg' => url,
+      'icon' => {
+        'type' => 'png',
+        'path' => 'icon.png'
+      }
+    }
+  end
+end
+
+puts JSON.generate({'items' => items}, :pretty_generate => true)
